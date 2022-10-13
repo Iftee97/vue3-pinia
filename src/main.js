a77b733664f3b272b1c44e445f26b80897ba2dd8
Iftee97 <tawsifmasrur@gmail.com>
2022-10-13 15:25:10 +0600

    ...

--- src/main.js
+++ src/main.js
@@ -7,4 +7,4 @@ import './assets/main.css'
 createApp(App).use(createPinia()).mount('#app')
 
 // start: npm run dev
-// start json server: json-server -w ./data/db.json
+// start json server: json-server --watch ./data/db.json --port 3000
